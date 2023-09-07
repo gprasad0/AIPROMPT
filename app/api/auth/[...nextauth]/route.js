f4504f966540738a16f8eede6c4f149aa57188ee
gprasad0 <guru96392@gmail.com>
2023-09-07 22:47:30 +0530

    added next config

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,39 +1,42 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import {connectToDb} from "@utils/database"
-import User from '@models/user'
-console.log("process.env.GOOGLE_ID",process.env.GOOGLE_ID)
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+import { connectToDb } from '@utils/database';
+import User from '@models/user';
+console.log('process.env.GOOGLE_ID', process.env.GOOGLE_ID);
 const handler = NextAuth({
-    providers:[
-        GoogleProvider({
-            clientId:process.env.GOOGLE_ID,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    async session({session}){
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
 
-    },
-    async signIn({profile}){
+    session.user.id = sessionUser._id.toString();
+  },
+  async signIn({ profile }) {
+    try {
+      await connectToDb();
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
 
-        try{
-            await connectToDb()
-            const userExists = await User.findOne({
-                email : profile.email
-            })
-
-            if(!userExists){
-                await User.create({
-                    email : profile.email,
-                    username : profile.name.replace(" ","").toLowerCase(),
-                    image : profile.picture
-                })
-                
-            }
-            return true
-        }catch(e){
-            console.log(e)
-            return false
-        }
-
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
     }
-})
+  },
+});
+
+export { handler as GET, handler as POST };
